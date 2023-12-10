b87ee3baf67ad46861fc2a037755441c0a7ca729
owin40 <116938413+owin40@users.noreply.github.com>
2023-12-10 12:19:54 +0700

    front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,14 +21,42 @@
           <li><a href="#home">Home</a></li>
           <li><a href="#event">Event</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a class="contact-button" href="#contact">Contact</a></li>
         </ul>
+      </div>
+      <div class="menu-bar">
+        <img src="{{ asset('images/menu.svg') }}" alt="">
       </div>
     </div>
 
     <div class="hero">
       <div class="header">
         <span>Event planner</span>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="sec2">
+    <div class="caro">
+      <div class="car1">
+        <img src="https://via.placeholder.com/300x300/808080" alt="">
+        <img src="https://via.placeholder.com/300x300/808080" alt="">
+        <img src="https://via.placeholder.com/300x300/808080" alt="">
+        <img src="https://via.placeholder.com/300x300/808080" alt="">
+      </div>
+    </div>
+  </section>
+
+  <section class="sec3">
+    <div class="about">
+      <div class="about-content">
+        <h2>Why Choose Us?</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam expedita itaque repudiandae hic labore quos tempore assumenda eveniet? Odio nihil rem ea laudantium fuga vitae. Assumenda enim optio repudiandae dolor!</p>
+        <div class="about-button"><a class="contact-button" href="#contact">Contact</a></div>
+      </div>
+      <div class="about-image">
+        <img src="https://via.placeholder.com/300x300/808080" alt="">
       </div>
     </div>
   </section>
