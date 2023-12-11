718450dbe5358e09ec2fd23f700d92ddf58cfe78
Pannacotta <andhikajayapura2016@gmail.com>
2023-12-12 04:28:23 +0700

    Breeze 1.1
    udh jd Admin Session, hehe

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/', 'index');
+Route::view('/admin', 'admin')->name('admin');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
