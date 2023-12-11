43f7538046f69f3c8740a22006beeff11c773f1c
Pannacotta <andhikajayapura2016@gmail.com>
2023-12-12 02:24:19 +0700

    Breeze 1.0
    Pls klo mw push kabarin, nabrak anj

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthManager;
-use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,47 +9,23 @@ use App\Http\Controllers\EventController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/index', function () {
-    return view('index');
-});
-
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/events', function () {
-    return view('events');
+    return view('welcome');
 });
 
-Route::get('/aboutus', function () {
-    return view('aboutus');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/team', function () {
-    return view('team');
-});
-
-Route::get('/contacts', function () {
-    return view('contacts');
-});
-
-Route::get('/login',[AuthManager::class, 'login'])->name('login');
-Route::post('/login',[AuthManager::class, 'loginPost'])->name('login.post');
-
-Route::get('/register',[AuthManager::class, 'regist'])->name('regist');
-Route::post('/register',[AuthManager::class, 'registPost'])->name('regist.post');
-Route::get('/regist',[AuthManager::class, 'regist'])->name('regist');
-Route::post('/regist',[AuthManager::class, 'registPost'])->name('regist.post');
-
-// routes/web.php
-Route::get('/events', [EventController::class, 'index'])->name('events.index');
-Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-Route::post('/events', [EventController::class, 'store'])->name('events.store');
-// ... other routes
-
-
+require __DIR__.'/auth.php';
