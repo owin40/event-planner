15374bd285c03531b70ccc491dd0174b2874d47a
Pannacotta <andhikajayapura2016@gmail.com>
2023-12-12 01:48:17 +0700

    Admin FORM
    backup Plan, hehe

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthManager;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,40 +9,23 @@ use App\Http\Controllers\AuthManager;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/index', function () {
-    return view('index');
+Route::view('/', 'index');
+Route::view('/admin', 'admin')->name('admin');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/events', function () {
-    return view('events');
-});
-
-Route::get('/aboutus', function () {
-    return view('aboutus');
-});
-
-Route::get('/team', function () {
-    return view('team');
-});
-
-Route::get('/contacts', function () {
-    return view('contacts');
-});
-
-Route::get('/login',[AuthManager::class, 'login'])->name('login');
-Route::post('/login',[AuthManager::class, 'loginPost'])->name('login.post');
-
-
-Route::get('/regist',[AuthManager::class, 'regist'])->name('regist');
-Route::post('/regist',[AuthManager::class, 'registPost'])->name('regist.post');
-
-Route::resource('nyobacrud', 'untukcrud');
+require __DIR__.'/auth.php';
