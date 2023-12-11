979590119fd9ae30915ee9426b1327110b2e9821
Pannacotta <andhikajayapura2016@gmail.com>
2023-12-12 04:50:03 +0700

    Breeze 1.3
    NEED BACKUP!!

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,5 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/register',[AuthManager::class, 'regist'])->name('regist');
+Route::post('/register',[AuthManager::class, 'registPost'])->name('regist.post');
+Route::get('/regist',[AuthManager::class, 'regist'])->name('regist');
+Route::post('/regist',[AuthManager::class, 'registPost'])->name('regist.post');
+
+// routes/web.php
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+Route::post('/events', [EventController::class, 'store'])->name('events.store');
+// ... other routes
 
 require __DIR__ . '/auth.php';
